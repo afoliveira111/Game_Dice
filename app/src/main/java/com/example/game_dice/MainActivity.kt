3026578b892f608ae0da095cac74ed135cbbb9b1
Felipe <felipetermometria@hotmail.com>
2024-01-26 13:52:33 -0300

    Create Click for Button

--- app/src/main/java/com/example/game_dice/MainActivity.kt
+++ app/src/main/java/com/example/game_dice/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.game_dice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val botao = findViewById<Button>(R.id.botao)
+        val textview = findViewById<TextView>(R.id.textview)
+        val imagem = findViewById<ImageView>(R.id.imagem)
+
+            botao.setOnClickListener {
+                val valor = (1..6).random()
+                textview.text = "Valor do dado = $valor"
+            }
+
     }
+
 }
