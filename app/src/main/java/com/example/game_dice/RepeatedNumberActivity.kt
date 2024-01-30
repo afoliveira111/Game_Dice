a0c18ce2632f26b96a08f97d015a888ba770d223
Felipe <felipetermometria@hotmail.com>
2024-01-30 15:37:51 -0300

    Create new activity

---
+++ app/src/main/java/com/example/game_dice/RepeatedNumberActivity.kt
@@ -0,0 +1,39 @@
+package com.example.game_dice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+
+class RepeatedNumberActivity : AppCompatActivity() {
+
+    private var ultimoValor = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val botao = findViewById<Button>(R.id.botao)
+        val textview = findViewById<TextView>(R.id.textview)
+        val imagem = findViewById<ImageView>(R.id.imagem)
+
+        botao.setOnClickListener {
+            val valor = (1..6).random()
+            textview.text = getString(R.string.valor_do_dado, valor)
+            when (valor) {
+                1 -> imagem.setImageResource(R.drawable.dice1)
+                2 -> imagem.setImageResource(R.drawable.dice2)
+                3 -> imagem.setImageResource(R.drawable.dice3)
+                4 -> imagem.setImageResource(R.drawable.dice4)
+                5 -> imagem.setImageResource(R.drawable.dice5)
+                6 -> imagem.setImageResource(R.drawable.dice6)
+            }
+            if (valor == ultimoValor) {
+                Toast.makeText(this, getString(R.string.valor_repetido), Toast.LENGTH_SHORT).show()
+            }
+            ultimoValor = valor
+        }
+    }
+}
