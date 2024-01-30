01b647fc7f822d7db0516a1e4f70dd558bc6ed5f
Felipe <felipetermometria@hotmail.com>
2024-01-30 17:49:22 -0300

    Update version. with animation.

--- app/src/main/java/com/example/game_dice/MainActivity.kt
+++ app/src/main/java/com/example/game_dice/MainActivity.kt
@@ -1,43 +1,67 @@
 package com.example.game_dice
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.Animator
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
 
     private var ultimoValor = -1
+    private lateinit var animationView: LottieAnimationView
+    private lateinit var imagem: ImageView
+    private lateinit var textview: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val botao = findViewById<Button>(R.id.botao)
-        val textview = findViewById<TextView>(R.id.textview)
-        val imagem = findViewById<ImageView>(R.id.imagem)
+        textview = findViewById(R.id.textview)
+        imagem = findViewById(R.id.imagem)
+        animationView = findViewById(R.id.animationView)
 
         botao.setOnClickListener {
-            val valor = (1..6).random()
-            textview.text = getString(R.string.valor_do_dado, valor)
-            when (valor) {
-                1 -> imagem.setImageResource(R.drawable.dice1)
-                2 -> imagem.setImageResource(R.drawable.dice2)
-                3 -> imagem.setImageResource(R.drawable.dice3)
-                4 -> imagem.setImageResource(R.drawable.dice4)
-                5 -> imagem.setImageResource(R.drawable.dice5)
-                6 -> imagem.setImageResource(R.drawable.dice6)
-            }
-            if (valor == ultimoValor) {
-                val intent = Intent(this, RepeatedNumberActivity::class.java)
-                intent.putExtra("number", valor)
-                startActivity(intent)
+            // Torna a imagem invisível antes de iniciar a animação
+            imagem.isVisible = false
+            // Inicia a animação quando o botão é clicado
+            animationView.isVisible = true
+            animationView.playAnimation()
+        }
+
+        animationView.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(p0: Animator) {}
+
+            override fun onAnimationEnd(p0: Animator) {
+                // A animação terminou, pode ser utilizado para alguma lógica se necessário
+                animationView.isVisible = false
+                // Exibe a imagem após a animação
+                exibirImagemDado()
             }
 
-            ultimoValor = valor
+            override fun onAnimationCancel(p0: Animator) {}
+
+            override fun onAnimationRepeat(p0: Animator) {}
+        })
+    }
+
+    private fun exibirImagemDado() {
+        val valor = (1..6).random()
+        textview.text = getString(R.string.valor_do_dado, valor)
+        when (valor) {
+            1 -> imagem.setImageResource(R.drawable.dice1)
+            2 -> imagem.setImageResource(R.drawable.dice2)
+            3 -> imagem.setImageResource(R.drawable.dice3)
+            4 -> imagem.setImageResource(R.drawable.dice4)
+            5 -> imagem.setImageResource(R.drawable.dice5)
+            6 -> imagem.setImageResource(R.drawable.dice6)
         }
+        // Torna a imagem visível após o término da animação
+        imagem.isVisible = true
+        ultimoValor = valor
     }
 }
